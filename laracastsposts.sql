afa09830b1b7a6ac3cc218f0518366a257a5b71b
Phellipe Lisboa <phellipelisboa.dev@gmail.com>
2023-12-19 13:40:17 -0300

    Updated Database
    Until ep 23

--- laracastsposts.sql
+++ laracastsposts.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 12/12/2023 às 18:09
+-- Tempo de geração: 19/12/2023 às 17:38
 -- Versão do servidor: 10.4.28-MariaDB
 -- Versão do PHP: 8.2.4
 
@@ -24,6 +24,29 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estrutura para tabela `notes`
+--
+
+CREATE TABLE `notes` (
+  `id` int(11) NOT NULL,
+  `body` text NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `notes`
+--
+
+INSERT INTO `notes` (`id`, `body`, `user_id`) VALUES
+(8, 'Ideias para férias', 3),
+(9, 'Pesquisa para o próximo trabalho de artes', 4),
+(10, 'Lembretes de trabalho', 3),
+(11, 'Técnicas de design', 4),
+(12, 'Sintaxe do PHP', 3);
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura para tabela `posts`
 --
 
@@ -39,6 +62,70 @@ CREATE TABLE `posts` (
 INSERT INTO `posts` (`id`, `title`) VALUES
 (1, 'First Blog Post'),
 (2, 'Second Blog Post');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`) VALUES
+(3, 'Phellipe', 'Phellipe@teste'),
+(4, 'John', 'John@teste');
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `notes`
+--
+ALTER TABLE `notes`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_notes_users` (`user_id`);
+
+--
+-- Índices de tabela `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `test2` (`email`) USING BTREE;
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `notes`
+--
+ALTER TABLE `notes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT de tabela `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Restrições para tabelas despejadas
+--
+
+--
+-- Restrições para tabelas `notes`
+--
+ALTER TABLE `notes`
+  ADD CONSTRAINT `FK_notes_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
